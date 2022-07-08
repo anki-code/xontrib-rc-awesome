c6988f0d6dbc10e8f8ca93097bfa0a4b79b50fad
Andy Kipp <anki-code@users.noreply.github.com>
2022-07-09 01:02:00 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import setuptools
+
+try:
+    with open('README.md', 'r', encoding='utf-8') as fh:
+        long_description = fh.read()
+except (IOError, OSError):
+    long_description = ''
+
+setuptools.setup(
+    name='xontrib-rc-awesome',
+    version='0.0.1',
+    license='MIT',
+    author='anki-code',
+    author_email='no@no.no',
+    description="Awesome snippets of code for xonshrc in xonsh shell.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    install_requires=[
+        'xonsh',
+        #'xontrib-prompt-bar',
+        #'xontrib-back2dir',
+        #'xontrib-sh',
+    ],
+    extras_require={
+        "xxh": ["xxh-xxh"],
+    },
+    packages=['xontrib'],
+    package_dir={'xontrib': 'xontrib'},
+    package_data={'xontrib': ['*.py', '*.xsh']},    
+    platforms='any',
+)
