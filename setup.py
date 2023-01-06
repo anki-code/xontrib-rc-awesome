d69d2f34d647d8c9f9eb25409fc6ffbc7440f48f
Andy Kipp <anki-code@users.noreply.github.com>
2023-01-06 18:49:50 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setuptools.setup(
     name='xontrib-rc-awesome',
-    version='0.2.4',
+    version='0.2.5',
     license='MIT',
     author='anki-code',
     author_email='no@no.no',
